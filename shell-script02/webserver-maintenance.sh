909acb583eecc9b56564f7e74d4adad7ac4bf622
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-01-28 12:27:54 -0300

    feat: Shell Script 02.
    Until class 03.

--- shell-script02/webserver-maintenance.sh
+++ shell-script02/webserver-maintenance.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_PATH="$(pwd)"
 LOG_SRC="$(pwd)/apache.log"
+TEMPLATE_EMAIL_APACHE=email_apache.tpl
+TEMPLATE_EMAIL_MEMORY=email_memory.tpl
+NOW=$(date +"%Y-%m-%dT%H-%M-%S")
 
 function search_subdir() {
     cd $1
@@ -51,11 +54,13 @@ apache_log() {
 apache_http_verb() {
     if [ -z $1 ];
     then
-        echo "*INVALID* parameter $1"
-        # TODO use read -p "Inform your HTTP verb: " choosed
-        choosed=""
-        verb=$(echo $choosed | awk '{ print toupper($1) }')
-        exit 1
+        while [ -z $choosed ]
+        do
+            echo "*INVALID* parameter $1"
+            read -p "Inform your HTTP verb: " choosed
+            verb=$(echo $choosed | awk '{ print toupper($1) }')
+        done
+        # exit 1
     else
         verb=$(echo $1 | awk '{ print toupper($1) }')
     fi
@@ -80,20 +85,58 @@ apache_http_verb() {
             done
             echo "--------"
             echo "TOTAL: $ITER"
-
-            if [ $1 == "GET" ]
-            then
-                echo true
-            elif [ $1 == "POST" ]
-            then
-                echo false
-            fi
-
             ;;
         *)
             echo "Invalid verb."
     esac
+}
 
+monitor_apache() {
+    site=$1
+    if [ -z $site ];
+    then
+        site="https://sorocaba.sp.gov.br"
+        echo "Using default site: $site"
+    fi
+
+    email_to=$2
+    if [ -z $email_to ];
+    then
+        email_to="TODO put some e-mail here"
+        echo "Using default e-mail to: $email_to"
+    fi
+
+
+    echo "Monitoring apache's status for $site (original: $1)"
+    status=$(curl --silent --write-out %{http_code} --output /dev/null $site)
+    echo "Returned status for $1 is $status"
+
+    if [ $status -ne 200 ];
+    then
+        echo "No good: status ($status) found in server. Warn everyone!!"
+        sed -i.bkp "s/^Apache returned code was:.*/Apache returned code was: $status [$NOW]/" $TEMPLATE_EMAIL_APACHE
+
+        ssmtp -v $email_to < $TEMPLATE_EMAIL_APACHE
+    fi
+}
+
+monitor_memory() {
+    echo "Monitoring memory"
+
+    total=$(free | grep -i mem | awk '{ print $2 }')
+    consumption=$(free | grep -i mem | awk '{ print $3 }')
+    used=$(bc <<< "scale=2;$consumption/$total*100" | awk -F. '{ print $1 }')
+    echo "Total used memory is: $used% ($consumption / $total * 100)"
+
+    email_to=$(cat $TEMPLATE_EMAIL_MEMORY | grep To: | awk -F: '{ print $2 }')
+    echo "Will send e-mail to: $email_to"
+
+    if [ $used -gt 30 ];
+    then
+        echo "Used too much memory!! Used is: $used% ($consumption / $total * 100)"
+        sed -i.bkp "s/^Memory consumption is:.*/Memory consumption is: $used% [$NOW]/" $TEMPLATE_EMAIL_MEMORY
+        ssmtp -v $email_to < $TEMPLATE_EMAIL_MEMORY
+    fi
 }
 
 case $1 in
@@ -102,6 +145,12 @@ case $1 in
         ;;
     "http_verb")
         apache_http_verb $2
+        ;;
+    "monitor_apache")
+        monitor_apache $2 $3
+        ;;
+    "memory")
+        monitor_memory
         ;;
     *)
         # TODO better usage message
