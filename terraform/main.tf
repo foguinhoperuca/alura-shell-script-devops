78fbeb4d059262e0a8148205bb59363d05da3eb1
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-07-14 10:08:54 -0300

    feat(terraform): Class 02.
    Access ssh is failing.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# TODO generate local ssh key to this project.
 resource "aws_instance" "dev" {
+  count = 3
+  # ami - image from ubuntu 24.04
   ami = "ami-020cba7c55df1f615"
   instance_type = "t2.micro"
-  key_name = "terraform-aws"
+  key_name = "Openshift-Key used everywhere"
+  tags = {
+      Name = "dev_${count.index}"
+  }
+  # vpc_security_group_ids = ["sg-0777fb748b6b6bcf5"]
+  vpc_security_group_ids = ["sg-0df47e9be592e7453"]
+}
+
+resource "aws_security_group" "access-ssh" {
+    name = "access-ssh"
+    description = "access-ssh"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["170.247.10.21/32"] # PMS 2025-07-14
+    }
+    egress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["170.247.10.21/32"] # PMS 2025-07-14
+    }
+    tags = {
+        Name = "access"
+    }
 }
