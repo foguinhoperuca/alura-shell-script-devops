5edf10b3a7205fc032b90e8cb583efdf74a7bfbd
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-07-15 08:48:09 -0300

    feat(terraform): Class 03.
    Finished.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,6 @@
 provider "aws" {
-  version = "~> 2.0"
+  # version = "~> 2.0" # version constraint inside of the provider is deprecated
   region = "us-east-1"
-}
-
-resource "aws_instance" "dev" {
-  count = 3
-  # ami - image from ubuntu 24.04
-  ami = "ami-020cba7c55df1f615"
-  instance_type = "t2.micro"
-  key_name = "Openshift-Key used everywhere"
-  tags = {
-      Name = "dev_${count.index}"
-  }
-  vpc_security_group_ids = ["sg-0777fb748b6b6bcf5"] aws-default: sg-0df47e9be592e7453
 }
 
 resource "aws_security_group" "access-ssh" {
@@ -34,5 +22,46 @@ resource "aws_security_group" "access-ssh" {
     }
     tags = {
         Name = "access"
+    }
+}
+
+resource "aws_instance" "dev" {
+  count = 3
+  # ami - image from ubuntu 24.04
+  ami = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+  key_name = "Openshift-Key used everywhere"
+  tags = {
+      Name = "dev_${count.index}"
+  }
+  vpc_security_group_ids = ["${aws_security_group.access-ssh.id}"] # aws-default: sg-0df47e9be592e7453; ${aws_security_group.access-ssh.id} will be convert into: sg-0777fb748b6b6bcf5
+}
+
+resource "aws_instance" "dev4" {
+  ami = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+  key_name = "Openshift-Key used everywhere"
+  tags = {
+      Name = "dev_4"
+  }
+  vpc_security_group_ids = ["${aws_security_group.access-ssh.id}"] # aws-default: sg-0df47e9be592e7453; ${aws_security_group.access-ssh.id} will be convert into: sg-0777fb748b6b6bcf5
+  depends_on = [aws_s3_bucket.bkt01]
+}
+
+resource "aws_instance" "dev5" {
+  ami = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+  key_name = "Openshift-Key used everywhere"
+  tags = {
+      Name = "dev_5"
+  }
+  vpc_security_group_ids = ["${aws_security_group.access-ssh.id}"] # aws-default: sg-0df47e9be592e7453; ${aws_security_group.access-ssh.id} will be convert into: sg-0777fb748b6b6bcf5
+}
+
+resource "aws_s3_bucket" "bkt01" {
+    bucket = "jecampos-bkt01"
+    acl = "private"
+    tags = {
+        Name = "jecampos-bkt01"
     }
 }
