f4b436401929018a58400914af3bb7018ff1db51
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-08-19 10:03:16 -0300

    feat(Kubernets01): Class 04.
    Finished.

---
+++ kubernets_01/send_message.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# FIXME put your correct Slack end point
+curl -X POST -H 'Content-type: application/json' --data '{"text": "Hello, a new pod was created!!"}' https://hooks.slack.com/services/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
+
