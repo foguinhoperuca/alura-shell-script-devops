b6f87573c407eb544b8d076573dacc6e0a82f6ce
Jefferson Campos <jecampos@sorocaba.sp.gov.br>
2025-08-20 09:10:45 -0300

    chores: Missing Update.
    Correct folder.

--- kubernets_01/stress.sh
+++ kubernets_01/stress.sh
@@ -3,7 +3,7 @@
 # TODO use siage to stress it 
 ADDRESS_SERVER=http://192.168.99.100:31595
 for i in {1..100000}; do
-  curl $ADDRESS_SERVER > test.txt
+  curl $ADDRESS_SERVER > output_stress/test.txt
   # In seconds. Pass something like 0.001 to really stress it
   sleep $1
 done
